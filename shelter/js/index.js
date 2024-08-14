03ee5ab4a527ab4b4059e71640643280f7b4e4fa
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-08-14 18:27:48 +0200

    feat: realization moving menu for main page(mobile-tablet size)

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,13 +1,56 @@
+
+let openMenu = false;
+const headerNav = document.querySelector('.header__nav');
+const buttonBurger = document.querySelector('.burger__nav');
+
+
+function measurementScreenSize() {
+    const windowInnerWidth = window.innerWidth;
+    if(windowInnerWidth >= 768){
+        console.log(windowInnerWidth);
+        openMenu = false;
+        if( document.querySelector('.close-menu')){
+            document.querySelector('.close-menu').classList.remove('close-menu');
+        }
+
+        if(document.querySelector('.menu')){
+            document.querySelector('.menu').classList.remove('menu');
+        }
+        
+        if(document.querySelector('.rotate')){
+            document.querySelector('.rotate').classList.remove('rotate');
+        }
+
+        if(document.querySelector('.nonrotate')){
+            document.querySelector('.nonrotate').classList.remove('rotate');
+        }
+    }
+}
+  
+window.addEventListener('resize', function() {
+    measurementScreenSize();
+});
+
+
+
+
+
 function controlBurgerMenu(){
     if(document.querySelector('.header')){
         const header = document.querySelector('.header');
-        const buttonBurger = document.querySelector('.burger__nav');
-        if(!document.querySelector('.rotate')){
-            buttonBurger.classList.remove('nonrotate')
-            buttonBurger.classList.add('rotate');
-        } else {
+       
+       if(!openMenu){
+            openMenu = true;
+            buttonBurger.classList.toggle('rotate');
+            headerNav.classList.toggle('menu');
+            headerNav.classList.remove('close-menu');
+            buttonBurger.classList.remove('nonrotate');
+       } else {
+            openMenu = false;
+            buttonBurger.classList.toggle('nonrotate');
+            headerNav.classList.toggle('close-menu');
+            headerNav.classList.remove('menu');
             buttonBurger.classList.remove('rotate');
-            buttonBurger.classList.add('nonrotate')
-        }
+       }
     }
 }
