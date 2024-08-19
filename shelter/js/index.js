f35edc3f956795235d29578844c8036e825217ef
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-08-19 13:34:10 +0200

    fix: fix css style for slider block

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -2,9 +2,13 @@
 import controlBurgerMenu from "./controlBurgerMenu.js";
 import { measurementScreenSize } from "./controlBurgerMenu.js";
 import { buttonBurger } from "./constants.js";
+import setGapSliderPetsBlock from "./slider.js";
+
+setGapSliderPetsBlock();
   
 window.addEventListener('resize', function() {
     measurementScreenSize();
+    setGapSliderPetsBlock();
 });
 
 buttonBurger.addEventListener('click', controlBurgerMenu);
--- shelter/js/slider.js
+++ shelter/js/slider.js
@@ -1,3 +1,15 @@
-export default function count() { 
-    console.log('click');
- }
+export default function setGapSliderPetsBlock() { 
+   const sliderPetsBlock = document.querySelector('.slider__pets-block');
+   console.log(sliderPetsBlock.clientWidth);
+   const sliderPetsBlockElements = document.querySelectorAll('.slider__block');
+   console.log(sliderPetsBlockElements);
+
+//    sliderPetsBlockElements.forEach((el)=> el.style.left = el.clientWidth + el.clientWidth)
+    // if(window.innerWidth >= 1261){
+    //     return sliderPetsBlock.style.gap = '90px';
+    // } else if(window.innerWidth <= 1260){
+    //     return sliderPetsBlock.style.gap = '59px';
+    // }
+
+
+}
