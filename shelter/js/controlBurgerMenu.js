f8676f9cc1667383b63a1fdefdda7171adf788e4
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-09-10 17:21:16 +0200

    feat: implementation of disabling the entire area except the burger menu when the burger menu is open

--- shelter/js/controlBurgerMenu.js
+++ shelter/js/controlBurgerMenu.js
@@ -2,6 +2,21 @@ import { headerNav, buttonBurger } from "./constants.js";
 
 let openMenu = false;
 
+const style = document.createElement('style');
+style.innerHTML = `
+  .my-element::before {
+    content: '';
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+  }
+`;
+
+
 export function measurementScreenSize() {
     const windowInnerWidth = window.innerWidth;
     if(windowInnerWidth >= 768){
@@ -29,14 +44,21 @@ export default function controlBurgerMenu(){
         openMenu = true;
         buttonBurger.classList.toggle('rotate');
         headerNav.classList.toggle('menu');
+        headerNav.style.set
         headerNav.classList.remove('close-menu');
         buttonBurger.classList.remove('nonrotate');
+        document.body.classList.add('no-scroll');
+        document.head.appendChild(style);
+        document.querySelector('.header').classList.add('my-element');
+     
     } else {
         openMenu = false;
         buttonBurger.classList.toggle('nonrotate');
         headerNav.classList.toggle('close-menu');
         headerNav.classList.remove('menu');
         buttonBurger.classList.remove('rotate');
+        document.body.classList.remove('no-scroll');
+        document.querySelector('.header').classList.remove('my-element')
     }
 }
 
